1565555e93c739d52ae5751428c127c2f4ca6c80
Anne Marie Jetton <annemariejetton@gmail.com>
2024-05-21 17:01:39 -0600

    navbar setup

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,27 @@
 import '../styles/NavBar.css';
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/AtoB.png'
+
 const NavBar = () => {
     return (
-        <div className='NavBar'>navbar</div>
+        <div className='NavBar'>
+            <Link to='/' className='NavBarLogo'>
+                <img src={logo}></img>
+            </Link>
+            <div className='NavBarLinks'>
+                <Link className='NavBarLink' to='/'>Fleet</Link>
+                <Link className='NavBarLink' to='/'>FAQ's</Link>
+                <Link className='NavBarLink' to='/'>Location</Link>
+                <Link className='NavBarLink' to='/'>Contact</Link>
+            </div>
+            <button className='NavBarButton'>
+                Book Now
+            </button>
+        </div>
     )
 }
 
