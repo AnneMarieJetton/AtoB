3a1cac674380529c5e5bc08cf79a9c2fd276452a
Anne Marie Jetton <annemariejetton@gmail.com>
2024-05-21 15:16:56 -0600

    Frontend setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
