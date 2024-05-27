477d459977c677fc65f44196501aeda1e106dccb
Anne Marie Jetton <annemariejetton@gmail.com>
2024-05-26 19:40:56 -0600

    5/26/24

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,71 @@
 import '../styles/Footer.css';
+import airportMap from '../assets/AtoB.png';
+import logo from '../assets/AtoB-removebg-preview.png';
 
 const Footer = () => {
     return (
-        <div className='Footer'>footer</div>
+        <div className='Footer'>
+            <div className='FooterImageContainer'>
+                <img src={logo}></img>
+            </div>
+            <div className='FooterInfoCol'>
+                <span className='FooterInfoColTitle'>Title</span>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>atobrentalsutah@gmail.com</span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>801-477-5067</span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>1331 Sky Wy, Provo, UT 84601</span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>Mon - Sat, 9am-7pm</span>
+                </div>
+            </div>
+            <div className='FooterInfoCol'>
+                <span className='FooterInfoColTitle'>Title</span>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>Facebook Link</span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>Facebook Link</span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span></span>
+                </div>
+                <div className='FooterInfoColLine'>
+                    <div>
+                        <img className='FooterImageContainer3' src={logo}></img>
+                    </div>
+                    <span>Mon - Sat, 9am-7pm</span>
+                </div>
+            </div>
+            <div className='FooterImageContainer2'>
+                <img src={airportMap}></img>
+            </div>
+        </div>
     )
 }
 
